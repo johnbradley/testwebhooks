e0f16f1349172d29fb4c5d026e4db75826720685
John Bradley <johnbradley2008@gmail.com>
2017-06-06 14:58:35 -0400

    deploy + github add

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 echo "HEY TRAVIS"
-env
+
+curl -i -X PUT -H "Authorization: token $COMMIT_TOKEN" -d '{"path": "test1.txt", "message": "Some Commit", "committer": {"name": "John Bradley", "email": "johnbradley2008@gmail.com"}, "content": "SEVZIHRoZXJlIGdpdGh1Ygo=", "branch": "master"}' https://api.github.com/repos/johnbradley/test-target-repo/contents/test1.txt
